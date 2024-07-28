b88bb91dab78ebf312cbc8e3abb7e7914b6d3b8f
MSaeed <mahmoudsaeed1996@gmail.com>
2024-07-28 11:42:00 +0300

    Display video with the ability to change playback rate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,6 +31,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final YoutubePlayerController _youtubePlayerController = YoutubePlayerController(
+    initialVideoId: YoutubePlayer.convertUrlToId("https://www.youtube.com/watch?v=0VzWgHkUYw4")!,
+    flags: const YoutubePlayerFlags(isLive: true),
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +43,34 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            YoutubePlayer(
+              controller: _youtubePlayerController,
+              bottomActions: [
+                ProgressBar(isExpanded: true),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Playback rates: '),
+                  const SizedBox(height: 8),
+                  ElevatedButton(onPressed: () => _youtubePlayerController.setPlaybackRate(1), child: const Text('x1')),
+                  ElevatedButton(onPressed: () => _youtubePlayerController.setPlaybackRate(1.25), child: const Text('x1.25')),
+                  ElevatedButton(onPressed: () => _youtubePlayerController.setPlaybackRate(1.5), child: const Text('x1.5')),
+                  ElevatedButton(onPressed: () => _youtubePlayerController.setPlaybackRate(1.75), child: const Text('x1.75')),
+                  ElevatedButton(onPressed: () => _youtubePlayerController.setPlaybackRate(2), child: const Text('x2')),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
